f2b7d79be6868e097dc16c5c484826bbd81c9e48
Glitch (cooked-melodious-dormouse) <none>
2020-05-03 12:52:55 +0000

    📡🙅 Checkpoint ./server.js:15705542/1083 ./views/index.html:15705542/38 ./package.json:15705542/37

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 
 var express = require('express');
 var cors = require('cors');
-
+const fs = require("fs"); 
 // require and use "multer"...
-
+var multer = require("multer");
+var upload = multer({dest:'uploads/'});
 var app = express();
 
 app.use(cors());
@@ -13,6 +14,17 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+//application
+
+//Load the filesystem module
+app.post('/api/fileanalyse',upload.single('upfile'),function(req,res){
+    // res.render('home',{fileSize: req.file.size});
+    fs.unlink(req.file.path,(err)=>{
+        if(err) console.log(err);});
+    res.json({fileSize: req.file.size})
+});
+
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
